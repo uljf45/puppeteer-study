79bdbb830fc85208ac94678318d9971fc4ca14df
HJS\chenzemin <chenzemin@hdfax.com>
2021-03-26 17:12:26 +0800

    1. 禁止某些js 文件 通过 cdp  Network.setBlockedURLs 2. promise.all 处理多个接口请求 3. 处理select下拉框选择 4. 异步执行不使用await 用then   await会阻塞主线程

--- investform/index.js
+++ investform/index.js
@@ -55,6 +55,8 @@ let site = 'http://dev.hdfax.com/v2/m/investmentFormInput/index.html'
     screenWidth: 375,
     width: 0,
   })
+
+  await client.send('Network.enable')
   await client.send('Network.setUserAgentOverride', {
     userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
   })
@@ -78,6 +80,9 @@ let site = 'http://dev.hdfax.com/v2/m/investmentFormInput/index.html'
   })
   await client.send('Page.enable')
   await client.send('Overlay.setShowHinge')
+  await client.send('Network.setBlockedURLs', { // 禁止某些js文件
+    urls: ["https://hm.baidu.com/hm.js?e994280905f4e78db10dfbb739e87ee0"]
+  })
 //cdp session 模拟浏览器打开手机模式 end
 
 
@@ -134,6 +139,121 @@ let site = 'http://dev.hdfax.com/v2/m/investmentFormInput/index.html'
   await page.waitForXPath(`//li[@class="prev_list_inner"]/div[string()="填写报单"]`)
 
   let createOrd = await page.$x(`//li[@class="prev_list_inner"]/div[string()="填写报单"]`)
-  createOrd[0].tap()
+  await createOrd[0].tap()
+  await page.waitForNavigation()
+
+  let [respBankInfo, respSellableProduct] = await Promise.all([  //判断多个接口请求
+    page.waitForResponse(resp => /op_query_recommender_bank_info/.test(resp.url())),
+    page.waitForResponse(resp => /op_query_sellable_resv_prod_quote_by_group_id/.test(resp.url())),
+  ])
+
+  let spRes = await respSellableProduct.json()
+  let prdName = spRes["resvProds"][0]["productName"]
+  console.log(prdName)
+
+  let div = await page.$x(`//span[string()="单据类型*"]/../div`)
+  await div[0].tap()
+
+  await page.waitForXPath(`//div[@class="modalContentLeft"]//span[string()="普通单据13"]`)
+  let billTypeSpan = await page.$x(`//div[@class="modalContentLeft"]//span[string()="普通单据13"]`)
+  await billTypeSpan[0].tap()
+  let confirmBtn = await page.$x(`//div[@class="modalFooter"]/span[string()="确定"]`)
+  await confirmBtn[0].tap()
+
+  let input = await page.$x(`//input[@placeholder="请填写投顾姓名"]`)
+  await input[0].type('毛颜')
+
+  input = await page.$x(`//input[@placeholder="请填写客户手机号"]`)
+  await input[0].type('13632545135')
+
+  input = await page.$x(`//input[@placeholder="请填写客户姓名"]`)
+  await input[0].type('陈泽敏')
+
+  input = await page.$x(`//input[@placeholder="请填写证件号码"]`)
+  await input[0].type('440582199005161810')
+
+  try {
+    page.waitForXPath('//div[@class="pop_name" and @style=""]/button').then(async () => { //异步执行, 不使用await await会阻塞主线程
+      let popNameKeHu = await page.$x('//div[@class="pop_name"]/button')
+      await popNameKeHu[0].tap()
+    })
+  } catch(err) {
+
+  }
+
+  // try {
+  //   page.waitForXPath('//div[@class="pop_name"]/button').then(async () => {
+  //     page.$x('//div[@class="pop_name"]/button').then((array) => {
+  //       array[0].tap()
+  //     })
+  //   })
+  // } catch(err) {
+
+  // }
+
+  let sel = await page.$x('//span[string()="是否有推荐人*"]/../select') // select下拉框选择
+  // await sel[0].tap()
+  await sel[0].select("0")  // select 选中下面的option 值为0
+  // let opt = await page.$x('//span[string()="是否有推荐人*"]/..//option[@value="0"]') //选否
+  // await opt[0].tap()
+
+  input = await page.$x(`//input[@placeholder="请输入产品名称"]`)
+  await input[0].type(prdName)
+
+  sel = await page.$x(`//li[string()="${prdName}"]`)
+  await sel[0].tap()
+
+  async function inputTypeByPlaceHoder(holderStr, val) {
+    let input = await page.$x(`//input[@placeholder="${holderStr}"]`)
+    await input[0].type(`${val}`)
+  }
+
+  input = await page.$x(`//input[@placeholder="请输入金额"]`)
+  await input[0].type('11')
+
+  input = await page.$x(`//span[string()="银行卡号*"]/../input[@placeholder="请填写银行卡号"]`)
+  await input[0].type(`123456789123456789`)
+
+  input = await page.$x(`//input[@placeholder="请输入银行名称"]`)
+  // await input[0].focus()
+  await input[0].type(`中国工商银行`, {
+    delay: 100
+  })
+  await page.waitForXPath(`//li[string()="中国工商银行"]`)
+  sel = await page.$x(`//li[string()="中国工商银行"]`)
+  await sel[0].tap()
+
+  await page.waitForResponse(resp => /op_query_bank_province_list/.test(resp.url()))
+  input = await page.$x(`//input[@placeholder="请输入开户行省份"]`)
+  await input[0].type(`广东省`, {
+    delay: 100
+  })
+  await page.waitForXPath(`//li[string()="广东省"]`)
+  sel = await page.$x(`//li[string()="广东省"]`)
+  await sel[0].tap()
+
+  await page.waitForResponse(resp => /op_query_bank_city_list/.test(resp.url()))
+  input = await page.$x(`//input[@placeholder="请输入开户行城市"]`)
+  await input[0].type(`深圳市`, {
+    delay: 100
+  })
+  await page.waitForXPath(`//li[string()="深圳市"]`)
+  sel = await page.$x(`//li[string()="深圳市"]`)
+  await sel[0].tap()
+
+  await page.waitForResponse(resp => /op_query_bank_branch_list/.test(resp.url()))
+  input = await page.$x(`//input[@placeholder="请输入开户行名称"]`)
+  await input[0].type(`中国工商银行深圳市分行`, {
+    delay: 100
+  })
+  await page.waitForXPath(`//li[string()="中国工商银行深圳市分行"]`)
+  sel = await page.$x(`//li[string()="中国工商银行深圳市分行"]`)
+  await sel[0].tap()
+  
+  sel = await page.$x('//span[string()="客户身份*"]/../select')
+  await sel[0].select("1")  // select 选中下面的option 值为0
+
+  console.log('done')
+  
 
 })()
